14c831040a7d59b2dad9e2e4ba508da33f4560b1
zesciati <zakyabiyyu2003@gmail.com>
2025-11-18 22:20:43 +0700

    feat: Menghubungkan elemen html to script.js = button dengan value .button-quote, quoteSentence dengan value .quotes, quoteNumber dengan value .quote-number. button menggunakan addeventlistener dengan click

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".button-quote");
+const quoteSentence = document.querySelector(".quotes");
+const quoteNumber = document.querySelector(".quote-number")
+
+
+
+
+button.addEventListener("click", getQuote);
