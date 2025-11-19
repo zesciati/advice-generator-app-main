04f5602e50d010f849cd6d8304765279e58be1af
zesciati <zakyabiyyu2003@gmail.com>
2025-11-19 21:40:20 +0700

    mengerjakan fungsi getQuote sampai line 15

--- script.js
+++ script.js
@@ -2,7 +2,23 @@ const button = document.querySelector(".button-quote");
 const quoteSentence = document.querySelector(".quotes");
 const quoteNumber = document.querySelector(".quote-number")
 
+async function getQuote() {
+  const quoteAPI = "https://api.adviceslip.com/advice";
 
+  button.disabled = true;
+  quoteSentence.style.opacity = 0
+  quoteNumber.style.opacity = 0; 
+
+  try{
+  const res = await fetch(quoteAPI, {cache: "no-store"});
+  const data = await res.json;
+
+  }catch{
+
+  }finally{
+
+  }
+}
 
 
 button.addEventListener("click", getQuote);
